05ca10e698d76996bb81cfe55064d1239f58d873
Jim Carroll <jim@pulamusic.com>
2019-09-21 11:52:23 -0400

    added some templates and Bootstrap code

--- flaskblog.py
+++ flaskblog.py
@@ -2,16 +2,32 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Jim Carroll',
+        'title': 'Blog Post 1',
+        'content': 'First post content: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'date_posted': 'September 20, 2019'
+    },
+    {
+        'author': 'James G. Carroll',
+        'title': 'Blog Post 2',
+        'content': 'Second post content: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'date_posted': 'September 21, 2019'
+    }
+]
+
 @app.route("/")
-def hello():
-    return "<h1>Home Page</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
     app.run(debug=True)
