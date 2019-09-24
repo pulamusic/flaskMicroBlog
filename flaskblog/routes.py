0eb3cb4488b0049c2cffce2351eb6c6650301215
Jim Carroll <jim@pulamusic.com>
2019-09-24 15:55:31 -0400

    resizing avatar images

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,7 @@
 import os
 import secrets
 
+from PIL import Image
 from flaskblog.models import User, Post
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
@@ -78,7 +79,11 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
 
     return picture_fn
 
